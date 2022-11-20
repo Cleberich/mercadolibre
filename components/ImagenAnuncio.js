6478cc28d17929fef60ea3d3863e026df413a9a3
Cleberich <97978772+Cleberich@users.noreply.github.com>
2022-11-20 17:28:56 -0300

    second commit

--- components/ImagenAnuncio.js
+++ components/ImagenAnuncio.js
@@ -3,8 +3,8 @@ import Image from "next/image"
 const ImagenAnuncio = () => {
   return (
     <div className="mx-3">
-        <Image 
-        src='/img/ImagenAnuncio.jpeg'
+         <Image 
+        src='/img/banner.jpeg'
         alt='banner'
         width={400}
         height={200}
