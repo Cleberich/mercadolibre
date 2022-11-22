8a9c17c706d1eabc1d95a9107320da5bb97d6908
Cleberich <97978772+Cleberich@users.noreply.github.com>
2022-11-22 14:09:23 -0300

    pagina de ver mas

--- components/Portada.js
+++ components/Portada.js
@@ -2,11 +2,25 @@ import Image from "next/image"
 
 const Portada = () => {
   return (
-    <div>
+    <div className="flex overflow-x-scroll">
         <Image 
         src='/img/banner.jpeg'
         className="w-full"
         alt='banner'
+        width={400}
+        height={200}
+        />
+        <Image 
+        src='/img/banner2.jpeg'
+        className="w-full"
+        alt='banner 2'
+        width={400}
+        height={200}
+        />
+        <Image 
+        src='/img/banner3.jpeg'
+        className="w-full"
+        alt='banner 3'
         width={400}
         height={200}
         />
--- pages/mas.js
+++ pages/mas.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import BarraInferior from "../components/BarraInferior";
 
 export default function mas() {
   return (
-    <div>mas</div>
+    <main className='bg-amarillo'>
+      <div className='bg-amarillo pt-36 pb-5 flex'>
+        <Link href='/'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-4 ml-2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+        </svg>
+        </Link>
+        <Image
+        src='/img/fotoUsuario.jpg'
+        alt='foto usuario'
+        className="user ml-7"
+        width={30}
+        height={30}
+        />
+        <h2 className="my-auto text-xl ml-7 font-boldito text-gray-800">Hola Cleber</h2>
+      </div>
+      <div className='block bg-white p-4'>
+        <div className='flex text-gray-700 my-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+          <Link className="mx-3" href='/'>Inicio</Link>
+        </div>
+        <div className='flex text-gray-700 my-6'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          <Link className="mx-3" href='/buesqueda'>Buscar</Link>
+        </div>
+        <div className='flex text-gray-700 my-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+          </svg>
+          <Link className="mx-3" href='/notificacion'>Notificaciones</Link>
+        </div>
+        <div className='flex text-gray-700 my-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+          </svg>
+          <Link className="mx-3" href='/miscompras'>Mis compras</Link>
+        </div>
+        <div className='flex text-gray-700 my-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+          </svg>
+          <Link className="mx-3" href='/favoritos'>Favoritos</Link>
+        </div>
+        <div className='flex text-gray-700 my-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6z" />
+          </svg>
+          <Link className="mx-3" href='/ofertas'>Ofertas</Link>
+        </div>
+        <div className='flex text-gray-700 my-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <Link className="mx-3" href='/historial'>Historial</Link>
+          <p className="bg-blue-500 text-xs  px-1 my-auto ml-60 font-bold rounded-md text-white">Nuevo</p>
+        </div>
+      </div>
+      <BarraInferior/>
+    </main>
   )
 }
