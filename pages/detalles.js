b3545e47d97ec353dd45000b5c39fc61cf7061e5
Cleberich <97978772+Cleberich@users.noreply.github.com>
2022-11-22 00:27:04 -0300

    pagina producto

--- pages/detalles.js
+++ pages/detalles.js
@@ -48,9 +48,9 @@ export default function Detalles() {
         <img className="w-80 mx-auto" src={detalle?.thumbnail} alt={detalle?.title}/>
       </div>
       <div className="pl-3">
-      <p className='line-through xs-1 text-gray-500'>{detalle ? formatearDinero( detalle.price * 1.12): ''}</p>
-      <p className='text-2xl my-auto'>{detalle ? formatearDinero( detalle.price): ''} <span className='text-verde font-bold text-sm pb-1'>12% OFF</span></p>
-      <p className='text-md'>en <span className='text-verde'>12 x {detalle ? formatearDinero( detalle.price/12): ''} sin interés</span></p>
+      <p className='line-through xs-1 text-gray-500'>{detalle ? formatearDinero(detalle?.price * 1.12): ''}</p>
+      <p className='text-2xl my-auto'>{detalle ? formatearDinero( detalle?.price): ''} <span className='text-verde font-bold text-sm pb-1'>12% OFF</span></p>
+      <p className='text-md'>en <span className='text-verde'>12 x {detalle ? formatearDinero(detalle?.price/12): ''} sin interés</span></p>
       <p className='xs-1 text-blue-600 mt-1'>Ver todos los medios de pago</p>
       <p className='bg-blue-500 rounded-sm w-20 mt-1 pl-1 xs text-white font-bold uppercase'>Oferta del día</p>
       </div>
