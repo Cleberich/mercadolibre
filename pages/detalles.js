614cd08f4a9882170192e76176db9c731f006d14
Cleberich <97978772+Cleberich@users.noreply.github.com>
2022-11-22 01:14:37 -0300

    pagina producto

--- pages/detalles.js
+++ pages/detalles.js
@@ -11,7 +11,7 @@ export default function Detalles() {
 
   return (
 <>
-    <div className='bg-white h-screen pb-44'>
+    <div className='bg-white h-screen'>
     <div className='bg-amarillo py-4 flex justify-between sticky top-0'>
       <div>
         <button type='button' className='ml-2' onClick={()=>router.push('/')}>
@@ -50,7 +50,7 @@ export default function Detalles() {
       </div>
       <div className="pl-3">
       <p className='line-through xs-1 text-gray-500'>${formatearDinero ? 213 :(detalle?.price * 1.12)}</p>
-      <p className='text-2xl my-auto'>${ formatearDinero(detalle ? 200 : detalle?.price)} <span className='text-verde font-bold text-sm pb-1'>12% OFF</span></p>
+      <p className='text-2xl my-auto'>{ formatearDinero(detalle ? 200 : detalle?.price)} <span className='text-verde font-bold text-sm pb-1'>12% OFF</span></p>
       <p className='text-md'>en <span className='text-verde'>10 x ${formatearDinero ? 123 :(detalle?.price/10)} sin interés</span></p>
       <p className='xs-1 text-blue-600 mt-1'>Ver todos los medios de pago</p>
       <p className='bg-blue-500 rounded-sm w-20 mt-1 pl-1 xs text-white font-bold uppercase'>Oferta del día</p>
@@ -99,6 +99,7 @@ export default function Detalles() {
       </div>
       </div>
       <BarraInferior/>
+      <div className='py-20'></div>
       </>
   )
 }
